3f5b24ea4fa164eaf88b23c09a8607de94b8fbef
Radensa <bangasaamv@gmail.com>
2025-08-12 10:23:55 +0700

    firs commit

---
+++ tugas 13.py/no4.py
@@ -0,0 +1,11 @@
+# Latihan 4: Slicing dan Penggabungan
+awal_minggu = ["Senin", "Selasa", "Rabu"]
+akhir_minggu = ["Kamis", "Jumat", "Sabtu", "Minggu"]
+
+#  list
+seminggu = awal_minggu + akhir_minggu
+
+# jadwal
+mulai_pendakian = seminggu[:5]
+
+print("mulai_pendakian:", mulai_pendakian)
